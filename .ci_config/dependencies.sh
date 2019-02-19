3537054179d8924cad3c58ef9ba17e381f274c0c
Artur Miller <amiller89@gmx.de>
2019-02-19 19:55:12 +0100

    Removed dependency to pybind11_catkin and added dependency to pybind11. This is needed to add dependencies to binder.

--- .ci_config/dependencies.sh
+++ .ci_config/dependencies.sh
@@ -1,3 +1,9 @@
 apt -qq install --no-install-recommends --allow-unauthenticated -y \
   build-essential python-catkin-tools python-pip protobuf-compiler
 pip install protobuf
+
+git clone https://github.com/pybind/pybind11.git
+cd pybind11
+cmake
+make install
+cd ..
