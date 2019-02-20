676b53a183d45da1316dd4ebbcadfaaa80231c5f
Artur Miller <amiller89@gmx.de>
2019-02-20 15:56:37 +0100

    Update dependencies.sh

--- .ci_config/dependencies.sh
+++ .ci_config/dependencies.sh
@@ -1,11 +1,12 @@
 apt -qq install --no-install-recommends --allow-unauthenticated -y \
   build-essential python-catkin-tools python-pip protobuf-compiler
 pip install protobuf
+pip install pytest
 
 cur_dir=$PWD
 cd ~/
 git clone https://github.com/pybind/pybind11.git
 cd pybind11
-cmake .
+cmake . -DPYTHON_EXECUTABLE=/usr/bin/python
 make install
 cd $cur_dir
