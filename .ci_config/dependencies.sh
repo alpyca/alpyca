749c59da4a2ebc2a99c79658d1dc3c606415a867
Artur Miller <amiller89@gmx.de>
2019-02-20 15:07:53 +0100

    Fixed read-only filesystem problem

--- .ci_config/dependencies.sh
+++ .ci_config/dependencies.sh
@@ -2,8 +2,10 @@ apt -qq install --no-install-recommends --allow-unauthenticated -y \
   build-essential python-catkin-tools python-pip protobuf-compiler
 pip install protobuf
 
+cur_dir=$PWD
+cd ~/
 git clone https://github.com/pybind/pybind11.git
 cd pybind11
 cmake
 make install
-cd ..
+cd $cur_dir
