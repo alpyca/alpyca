6de935e38a06573e6f6dd96542ac077dd43326da
Artur Miller <amiller89@gmx.de>
2019-03-02 22:55:28 +0100

    Type conversion is now determined by the sensor type

--- alpyca_sim/src/alpyca/sim/plugin_runner.cpp
+++ alpyca_sim/src/alpyca/sim/plugin_runner.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <iostream>
+#include <stdexcept>
 #include <ros/console.h>
 #include "alpyca/sim/plugin_runner.h"
 
@@ -24,7 +25,15 @@ void PluginRunner::Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
   /*
    Wrap the call of the Load function of the Python plugin.
   */
-  sensor_wrapper = new ContactSensorWrapper(_sensor);
+  std::string plugin_type = _sensor->Type();
+  if(plugin_type == "contact")
+  {
+    sensor_wrapper = new ContactSensorWrapper(std::dynamic_pointer_cast<sensors::ContactSensor>(_sensor));
+  }
+  else
+  {
+    throw std::invalid_argument( "Unknown base_plugin!" );
+  }
   // Start the interpreter and keep it alive
   guard = new py::scoped_interpreter();
   
