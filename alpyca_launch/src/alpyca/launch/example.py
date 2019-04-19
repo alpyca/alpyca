a31d4875a0b8405ec7d5bb1ea4156730cc73de09
Artur Miller <amiller89@gmx.de>
2019-04-19 15:49:33 +0200

    Initial commit of alpyca_launch

---
+++ alpyca_launch/src/alpyca/launch/example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from __future__ import division, absolute_import, print_function
+
+from alpyca.launch import Launcher
+
+
+def main():
+    launcher = Launcher()
+
+if __name__ == "__main__":
+    main()
---
+++ alpyca_launch/src/alpyca/launch/launcher.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from __future__ import division, absolute_import, print_function
+
+
+__all__ = ['Launcher']
+
+class Launcher(object):
+
+    def __init__(self):
+        pass
