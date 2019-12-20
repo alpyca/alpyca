8b35b6dbe69c3b546a7e66517c1b97baa6ef5ad3
Artur Miller <amiller89@gmx.de>
2019-12-20 20:53:19 +0100

    Deleted not needed dependencies

--- .ci_config/dependencies.sh
+++ .ci_config/dependencies.sh
@@ -1,17 +1,7 @@
-apt -qq install --no-install-recommends --allow-unauthenticated -y \
+apt -qq install --no-install-recommends -y \
   build-essential python-catkin-tools python-pip protobuf-compiler \
   python-dev
 
-pip install wheel
 pip install protobuf
-pip install pytest
 pip install robotpy-cppheaderparser
 pip install Jinja2
-
-cur_dir=$PWD
-cd ~/
-git clone https://github.com/pybind/pybind11.git
-cd pybind11
-cmake . -DPYTHON_EXECUTABLE=/usr/bin/python
-make install
-cd $cur_dir
